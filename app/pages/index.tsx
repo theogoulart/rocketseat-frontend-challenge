4f8963a6ad71beadc47d4964b84e3368fbd0d038
Theo Goulart <theo_goulart@hotmail.com>
2022-09-15 10:58:34 -0300

    add next.js, typescript and styled-components

---
+++ app/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Caputeeno Store</title>
+        <meta name="description" content="Compre camisas e acessórios!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  )
+}
