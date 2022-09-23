d53c68a20c7e158447a5a4bfa5dfd30c5d3946c4
Theo Goulart <theo_goulart@hotmail.com>
2022-09-23 13:30:04 -0300

    add apollo client

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,3 +1,6 @@
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+
 import Head from 'next/head'
 import Product from '../components/ProductCard'
 import Pagination from '../components/Pagination'
@@ -35,58 +38,7 @@ const FlexBar = styled.div`
   width: 100%;
 `
 
-export default function Home() {
-  const data = [
-    {
-      "id": "1c19939f-9506-4fc3-b11e-269ad0b939fb",
-      "price_in_cents": 3689,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-06.jpg",
-      "name": "Camiseta Outcast"
-    },
-    {
-      "id": "3fdfecbc-281f-412d-95c9-f35127957c8f",
-      "price_in_cents": 6802,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/caneca-03.jpg",
-      "name": "Caneca Never settle"
-    },
-    {
-      "id": "f93954f4-e226-4b8f-8a93-9ef001939e2a",
-      "price_in_cents": 6235,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/caneca-06.jpg",
-      "name": "Caneca de cerâmica rústica"
-    },
-    {
-      "id": "4c48e342-726f-423c-b346-3cf3869710ca",
-      "price_in_cents": 2039,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-04.jpg",
-      "name": "Camiseta Ramones"
-    },
-    {
-      "id": "9eed490d-b8a9-446c-aaf9-d95c8f699c59",
-      "price_in_cents": 5858,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-02.jpg",
-      "name": "Camiseta evening"
-    },
-    {
-      "id": "ead0d77b-8c61-4f2d-8978-a9551983bb0f",
-      "price_in_cents": 2791,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/caneca-06.jpg",
-      "name": "Caneca de cerâmica rústica"
-    },
-    {
-      "id": "d54e8ca0-8573-4798-8652-c3c9def3257b",
-      "price_in_cents": 8476,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/camiseta-05.jpg",
-      "name": "Camiseta not today."
-    },
-    {
-      "id": "4ceff697-ef19-401a-b23e-e26541d1aacc",
-      "price_in_cents": 6267,
-      "image_url": "https://storage.googleapis.com/xesque-dev/challenge-images/caneca-03.jpg",
-      "name": "Caneca Never settle"
-    }
-  ];
-
+export default function Home({products}) {
   return (
     <div>
       <Head>
@@ -103,7 +55,7 @@ export default function Home() {
           </FlexBar>
           <Pagination/>
           <Grid>
-            {data.map((item, i) => (<Product key={i} product={item} />))}
+            {products.map((item, i) => (<Product key={i} product={item} />))}
           </Grid>
           <Pagination/>
         </Container>
@@ -112,4 +64,25 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const  { data, error } = await client.query({
+    query: gql`
+      query products {
+        allProducts(perPage: 12, page: 1) {
+          id
+          image_url
+          name
+          price_in_cents
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      products: data.allProducts,
+    },
+  };
 }
