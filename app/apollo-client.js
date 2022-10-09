636efb5f2817ed8621429ced864acf6c348ab25a
Theo Goulart <theo_goulart@hotmail.com>
2022-10-08 21:24:53 -0300

    update .env

--- app/apollo-client.js
+++ app/apollo-client.js
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+console.log(process.env);
+console.log(process.env.NEXT_PUBLIC_API_URI);
 const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_API_URL,
+    uri: process.env.NEXT_PUBLIC_API_URI,
     cache: new InMemoryCache(),
 });
 
