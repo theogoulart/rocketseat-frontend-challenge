cd2a1df960d5ba6970ad5f57f0e8fcf2c8392928
Theo Goulart <theo_goulart@hotmail.com>
2022-10-08 21:31:12 -0300

    remove .env

--- app/apollo-client.js
+++ app/apollo-client.js
@@ -1,7 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-console.log(process.env);
-console.log(process.env.NEXT_PUBLIC_API_URI);
 const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_API_URI,
     cache: new InMemoryCache(),
