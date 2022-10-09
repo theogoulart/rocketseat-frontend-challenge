6934da3d2404e60c855fc57504b0a4fb0efc0022
Theo Goulart <theo_goulart@hotmail.com>
2022-10-08 21:34:23 -0300

    add debbuging

--- app/apollo-client.js
+++ app/apollo-client.js
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+console.log(process.env.NEXT_PUBLIC_API_URI);
 const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_API_URI,
     cache: new InMemoryCache(),
